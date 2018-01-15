84c2fe9b8d612dbad452916479b6a762dfe9ffb3
Diego Najar <dignajar@gmail.com>
2018-01-15 22:15:10 +0100

    Minor changes on footer

--- php/footer.php
+++ php/footer.php
@@ -1,10 +1,21 @@
 <?php defined('BLUDIT') or die('Bludit'); ?>
 <footer class="py-4 bg-dark">
 	<div class="container">
+	<div class="row mb-2">
+			<div class="col-6">
+			<a class="text-white" href="<?php echo $_topbar['website'] ?>">Bludit Themes</a>
+				<span class="text-muted">© 2018</span>
+			</div>
+			<div class="col-6 text-right">
+				<a class="text-white" href="https://themes.bludit.com">English</a>
+				<span class="text-muted"> | </span>
+				<a class="text-white" href="https://themes.bludit.com/de/">Deutsch</a>
+				<span class="text-muted"> | </span>
+				<a class="text-white" href="https://themes.bludit.com/es/">Español</a>
+			</div>
+		</div>
 		<div class="row">
 			<div class="col-6">
-				<a class="text-white" href="<?php echo $_topbar['website'] ?>">Bludit Themes</a>
-				<span class="text-muted">© 2018</span>
 			</div>
 			<div class="col-6 text-right">
 				<a class="text-white" href="https://www.bludit.com">Official Bludit Site</a>
@@ -16,5 +27,6 @@
 				<a class="text-white" href="https://www.bludit.com">Add New Theme</a>
 			</div>
 		</div>
+
 	</div>
 </footer>
