fd8132d80f996244773c5a1751cd39da98376467
Diego Najar <dignajar@gmail.com>
2018-01-23 20:44:40 +0100

    Lang updated

--- html/topnavbar.php
+++ html/topnavbar.php
@@ -4,7 +4,7 @@
 		<a class="navbar-brand" href="<?php echo $_topbar['website'] ?>">
 			<img src="<?php echo DOMAIN ?>/img/bludit-logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
 			<span class="text-white">BLUDIT</span>
-			<span class="ml-1 text-muted"><?php l('Plugins') ?></span>
+			<span class="ml-1 text-muted"><?php l('Themes') ?></span>
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
