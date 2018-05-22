56db8aa29a5f709f657a6920013722e055a5ca56
Diego Najar <dignajar@gmail.com>
2018-05-22 17:17:44 +0200

    Update head.php

--- html/head.php
+++ html/head.php
@@ -65,14 +65,4 @@
 
 	<!-- Canonical URL -->
 	<link rel="canonical" href="<?php echo $_canonicalURL ?>">
-
-	<!-- Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo GOOGLE_ANALYTICS ?>"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-		gtag('config', '<?php echo GOOGLE_ANALYTICS ?>');
-	</script>
-
 </head>
