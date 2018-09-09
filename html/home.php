06e9d239d2dc27a6a5a99276ef33d05b2984ae22
Diego Najar <dignajar@gmail.com>
2018-09-09 14:55:52 +0200

    improves for bludit v3.x

--- html/home.php
+++ html/home.php
@@ -26,7 +26,7 @@
 						} elseif ($item['price_usd']==-1) {
 							echo '<a class="btn btn-secondary btn-sm" href="'.$item['download_url'].'" role="button" target="_blank"><i class="fa fa-shopping-cart" aria-hidden="true"></i> '.l('Buy from',false).' $1</a>'.PHP_EOL;
 						} else {
-							echo '<a class="btn btn-secondary btn-sm" href="'.$item['permalink'].'#download" role="button" target="_blank"><i class="fa fa-download" aria-hidden="true"></i> '.l('Download',false).'</a>'.PHP_EOL;
+							echo '<a class="btn btn-secondary btn-sm" href="'.$item['permalink'].'#download" role="button"><i class="fa fa-download" aria-hidden="true"></i> '.l('Download',false).'</a>'.PHP_EOL;
 						}
 					?>
 				</div>
