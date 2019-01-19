20e8ddf928aa6d24896ede1ea152480fb5e452f6
Diego Najar <dignajar@gmail.com>
2019-01-19 23:53:55 +0100

    Update head.php

--- html/head.php
+++ html/head.php
@@ -7,8 +7,8 @@
 
 	<?php
 		if ($_item!==false) {
-			echo '<title>'.htmlentities($_item['name'], ENT_QUOTES).' - '.l('head title', false).'</title>'.PHP_EOL;
-			echo '<meta name="description" content="'.htmlentities($_item['description'], ENT_QUOTES).'">'.PHP_EOL;
+			echo '<title>'.htmlentities(strip_tags($_item['name']), ENT_QUOTES).' - '.l('head title', false).'</title>'.PHP_EOL;
+			echo '<meta name="description" content="'.htmlentities(strip_tags($_item['description']), ENT_QUOTES).'">'.PHP_EOL;
 		} else {
 			echo '<title>'.l('head title', false).'</title>'.PHP_EOL;
 			echo '<meta name="description" content="'.l('head description', false).'">'.PHP_EOL;
@@ -23,9 +23,9 @@
 	<meta name="twitter:site" content="@bludit" />
 	<?php
 		if ($_item!==false) {
-			echo '<meta name="twitter:title" content="'.htmlentities($_item['name'], ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
-			echo '<meta name="twitter:description" content="'.htmlentities($_item['description'], ENT_QUOTES).'" />'.PHP_EOL;
-			echo '<meta name="twitter:image" content="'.$_item['screenshot_twitter_url'].'" />'.PHP_EOL;
+			echo '<meta name="twitter:title" content="'.htmlentities(strip_tags($_item['name']), ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
+			echo '<meta name="twitter:description" content="'.htmlentities(strip_tags($_item['description']), ENT_QUOTES).'" />'.PHP_EOL;
+			echo '<meta name="twitter:image" content="'.$_item['screenshoot_twitter_url'].'" />'.PHP_EOL;
 		} else {
 			echo '<meta name="twitter:title" content="'.l('head title', false).'" />'.PHP_EOL;
 			echo '<meta name="twitter:description" content="'.l('head description', false).'" />'.PHP_EOL;
@@ -36,20 +36,21 @@
 	<!-- Open Graph -->
 	<meta property="og:locale" content="<?php echo $currentLanguage ?>" />
 	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="Bludit Plugins"/>
 	<meta property="og:image:type" content="image/png" />
-	<meta property="og:image:alt" content="Bludit Logo" />
-	<meta property="og:site_name" content="Bludit Themes"/>
 	<?php
 		if ($_item!==false) {
 			echo '<meta property="og:url" content="'.$_item['permalink'].'" />'.PHP_EOL;
-			echo '<meta property="og:title" content="'.htmlentities($_item['name'], ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
-			echo '<meta property="og:description" content="'.htmlentities($_item['description'], ENT_QUOTES).'" />'.PHP_EOL;
-			echo '<meta property="og:image" content="'.$_item['screenshot_facebook_url'].'" />'.PHP_EOL;
+			echo '<meta property="og:title" content="'.htmlentities(strip_tags($_item['name']), ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
+			echo '<meta property="og:description" content="'.htmlentities(strip_tags($_item['description']), ENT_QUOTES).'" />'.PHP_EOL;
+			echo '<meta property="og:image" content="'.$_item['screenshoot_facebook_url'].'" />'.PHP_EOL;
+			echo '<meta property="og:image:alt" content="'.htmlentities(strip_tags($_item['name']), ENT_QUOTES).'" />'.PHP_EOL;
 		} else {
 			echo '<meta property="og:url" content="'.$_topbar['website'].'" />'.PHP_EOL;
 			echo '<meta property="og:title" content="'.l('head title', false).'" />'.PHP_EOL;
 			echo '<meta property="og:description" content="'.l('head description', false).'" />'.PHP_EOL;
 			echo '<meta property="og:image" content="https://df6m0u2ovo2fu.cloudfront.net/images/bludit-facebook-cards.png" />'.PHP_EOL;
+			echo '<meta property="og:image:alt" content="Bludit Logo" />'.PHP_EOL;
 		}
 	?>
 
