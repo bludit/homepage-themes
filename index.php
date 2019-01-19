a473624d14e678d9c1efa9eaf7aaf5888917fd4b
Diego Najar <dignajar@gmail.com>
2019-01-20 00:02:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 	<!-- Javascript stuff -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
 
 	<script>
 	$(document).ready(function() {
@@ -36,5 +36,24 @@
 	});
 	</script>
 
+	<!--
+		Yandex.Metrika counter
+		Masking IP addresses enabled for GDPR
+	-->
+	<script type="text/javascript" >
+	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	   ym(51983243, "init", {
+		id:51983243,
+		clickmap:true,
+		trackLinks:true,
+		accurateTrackBounce:true
+	   });
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/51983243" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+	
 </body>
 </html>
